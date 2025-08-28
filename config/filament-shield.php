4ee4b579a3d4312ed71f9ce519bf7c3358bc1cb6
Amrullah Solikun <amrullahs@gmail.com>
2025-08-28 16:46:00 +0700

    dashboard

--- config/filament-shield.php
+++ config/filament-shield.php
@@ -73,7 +73,7 @@ return [
         ],
 
         'widgets' => [
-            'AccountWidget', 'FilamentInfoWidget',
+            // 'AccountWidget', 'FilamentInfoWidget',
         ],
 
         'resources' => [],
